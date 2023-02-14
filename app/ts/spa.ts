c97fc9bb217ba49837056d50e0fb085a86465aed
AnDev58 <andev58@outlook.com>
2023-02-14 15:48:03 +0300

    modified:   app/ts/spa.ts

--- app/ts/spa.ts
+++ app/ts/spa.ts
@@ -1,41 +1,28 @@
 interface Route {
     path: string,
-    component: string
+    component: HTMLElement
 }
-interface Options {
-    root?: string,
-    routes?: Route[],
-}
-class Router { 
-    private routes: Route[] = [];
-    private root = "/"
+
+class Router {
+    private _routes: Route[] = []
+    public get routes() : Route[] {
+        return this._routes
+    }
     
-    /**
-     *
-     */
-    constructor(options: Options) {
-        if (options.root) {
-            this.root = options.root
-        }
-        if (options.routes) {
-            this.routes = options.routes
-        }
+    private root = '/'
+
+    constructor(root ?: string) {
+        if (root) this.root = root
     }
 
-    /**
-     * add
-     */
-    public addRoute(route: Route) {
-        this.routes.push(route)
+    add(path: string, component: HTMLElement) {
+        this._routes.push({path: path, component: component})
         return this
     }
-    /**
-     * add
-     */
-    public add(path: string, component: string) {
-        this.routes.push({
-            path,
-            component
-        })
+
+    remove(path: string) {
+        
     }
 }
+
+export default Router
